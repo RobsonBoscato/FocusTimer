818fef6b4174a3ebf555b2f870054d83371a0787
RobsonBoscato <rbsn.boscato@gmail.com>
2022-09-02 14:57:36 -0300

    turned functional buttons

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ const buttonStop = document.querySelector('.stop');
 const buttonSet = document.querySelector('.set');
 const buttonSoundOn = document.querySelector('.sound-on');
 const buttonSoundOff = document.querySelector('.sound-off');
-let minutes;
+let timerTimeOut;
 const minutesDisplay = document.querySelector('#minutes');
 const secondsDisplay = document.querySelector('#seconds');
+
+let minutes = Number(minutesDisplay.textContent);
 
 buttonPlay.addEventListener('click', handleButtonPlay);
 buttonPause.addEventListener('click', handleButtonPause);
@@ -16,14 +18,14 @@ buttonSoundOff.addEventListener('click', handleButtonSOundOff);
 buttonSet.addEventListener('click', handleButtonSet);
 
 function countdown() {
-  setTimeout(function () {
+  timerTimeOut = setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent);
     let minutes = Number(minutesDisplay.textContent);
 
     updateTimerDisplay(minutes, 0);
 
     if (minutes <= 0) {
-      resetTimer();
+      resetControls();
       return;
     }
 
@@ -42,11 +44,16 @@ function updateTimerDisplay(minutes, seconds) {
   secondsDisplay.textContent = String(seconds).padStart(2, '0');
 }
 
-function resetTimer() {
+function resetControls() {
   buttonPlay.classList.remove('hide');
   buttonPause.classList.add('hide');
   buttonSet.classList.remove('hide');
   buttonStop.classList.add('hide');
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0);
+  clearTimeout(timerTimeOut);
 }
 
 function handleButtonPlay() {
@@ -61,10 +68,14 @@ function handleButtonPlay() {
 function handleButtonPause() {
   buttonPlay.classList.remove('hide');
   buttonPause.classList.add('hide');
+
+  clearTimeout(timerTimeOut);
 }
 
 function handleButtonStop() {
+  resetControls();
   resetTimer();
+  console.log(minutes);
 }
 
 function handleButtonSOundOn() {
@@ -77,7 +88,24 @@ function handleButtonSOundOff() {
   buttonSoundOff.classList.add('hide');
 }
 
+function inputValidation(n1) {
+  console.log(n1);
+  if (n1 => 1 || n1 == null) {
+  } else {
+    return false;
+  }
+  if (n1 == isNaN) {
+    alert('Dados inseridos inválidos, insira um valor inteiro');
+  }
+}
+
 function handleButtonSet() {
-  let timerMinutes = prompt('Quantos minutos? ');
+  let timerMinutes = prompt('Quantos minutos? ') || 0;
+
+  if (!timerMinutes) {
+    resetTimer();
+    return;
+  }
+  minutes = timerMinutes;
   updateTimerDisplay(timerMinutes, 0);
 }
